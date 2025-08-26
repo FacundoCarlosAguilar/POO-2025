86f0d35163690302a603a3a4c6b0aff2830a3974
FacundoCarlosAguilar <faguilar@alumnos.unsada.edu.ar>
2025-08-26 20:32:01 -0300

    Commit

--- Actividad1/MainColegio.java
+++ Actividad1/MainColegio.java
@@ -1,7 +1,6 @@
 package Actividad1;
 public class MainColegio {
     public static void main(String[] args) {
-        
 
         Alumno alum1 = new Alumno();
         Docente docente1 = new Docente();
@@ -9,7 +8,6 @@ public class MainColegio {
 
         System.out.println("El nombre del Alumno: " + alum1.getNombreAlumno());
         System.out.println("El alumno " + alum1.getNombreAlumno() + " tiene de profesor:  " + docente1.getNombreDocente());
-        System.out.println("El tipo de asignatura que enseña " + docente1.getNombreDocente() + " es: " + asignat1.getNombreMateria());
-        
+        System.out.println("El tipo de asignatura que enseña " + docente1.getNombreDocente() + " es: " + asignat1.getNombreMateria());       
      }
 }
